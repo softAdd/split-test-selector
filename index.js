18ab0ddfbbcb85213f93082d844f54ce3a89f011
Vasily Mikhaylovsky <git@3apaxi.com>
2019-11-15 15:35:31 +0500

    using commonjs instead of es6 modules

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
  * @return {Number} index
  */
 
-function NOD (arr) {
-  return arr.reduce(function ( x, y ) {
+function NOD(arr) {
+  return arr.reduce(function (x, y) {
     while (true) {
       if (y === 0)
         return x;
@@ -19,35 +19,35 @@ function NOD (arr) {
   });
 }
 
-function sumElems (arr) {
+function sumElems(arr) {
   return arr.reduce((result, value) => result + value);
 }
 
-export default function splitTestSelector(visit, percentages) {
+module.exports = function splitTestSelector(visit, percentages) {
   //sort percentages
   let values = percentages
-  .map((percent, index) => ({
-    percent,
-    index
-  }))
-  .sort((a, b) => {
-    if (a.percent === b.percent) {
-      return a.index - b.index
-    }
-    if (a.percent < b.percent) {
-      return 1
-    } else {
-      return -1
-    }
-  });
+    .map((percent, index) => ({
+      percent,
+      index
+    }))
+    .sort((a, b) => {
+      if (a.percent === b.percent) {
+        return a.index - b.index
+      }
+      if (a.percent < b.percent) {
+        return 1
+      } else {
+        return -1
+      }
+    });
 
   let percentagesOverall = sumElems(percentages);
-  let usersOnePass = percentagesOverall/NOD(percentages);
+  let usersOnePass = percentagesOverall / NOD(percentages);
   let currentLap = Math.ceil(visit / usersOnePass);
   let currentUser = usersOnePass - ((usersOnePass * currentLap) - visit);
-  
+
   for (let value of values) {
-    currentUser -= Math.ceil(usersOnePass * value.percent/percentagesOverall);
+    currentUser -= Math.ceil(usersOnePass * value.percent / percentagesOverall);
     if (currentUser <= 0) {
       return value.index;
     }
