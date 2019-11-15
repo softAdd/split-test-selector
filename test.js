ee76ec186470f6c333c485b1588a94b9877f88d1
Vasily Mikhaylovsky <git@3apaxi.com>
2019-11-15 15:32:12 +0500

    49/51

--- test.js
+++ test.js
@@ -36,6 +36,40 @@ test("simple", t => {
   }
 });
 
+test("49%/51%", t => {
+  const variants = [49, 51];
+
+  {
+    const variant = splitTestSelector(1, variants);
+    t.is(variant, 2, "visit #1");
+  }
+  {
+    const variant = splitTestSelector(2, variants);
+    t.is(variant, 2, "visit #2");
+  }
+  {
+    const variant = splitTestSelector(3, variants);
+    t.is(variant, 1, "visit #3");
+  }
+});
+
+test("51%/49%", t => {
+  const variants = [51, 49];
+
+  {
+    const variant = splitTestSelector(1, variants);
+    t.is(variant, 1, "visit #1");
+  }
+  {
+    const variant = splitTestSelector(2, variants);
+    t.is(variant, 1, "visit #2");
+  }
+  {
+    const variant = splitTestSelector(3, variants);
+    t.is(variant, 2, "visit #3");
+  }
+});
+
 test("two paralel", t => {
   const variants1 = [50, 25, 25];
   const variants2 = [10, 5, 85];
